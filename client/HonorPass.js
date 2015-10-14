e979c37b3674afd04b935237f79a396f64044e18
PRASAD Kanha <41562@2017-41562.local>
2015-10-15 00:56:51 +0700

    Long way to go

--- client/HonorPass.js
+++ client/HonorPass.js
@@ -1,14 +1,16 @@
 /*
 TODO:
-1. Fix upvoting feature
-2. Fix time
+date thingies still not fully complete; focus on that later.
+3. Fix upvoting feature
 3. Testing + fix errors in cannot find property etc.
-4. Designing frontend
+4. Move onto designing frontend
 */
+
+
 Template.main.helpers({
   'thingies': function(){
     //return list of posts sorted by vote
-    return Thingies.find({},{sort:{vote:-1}});
+    return Thingies.find({},{sort:{vote:-1}})
   },
   'yours' : function (){
     try{
@@ -24,23 +26,14 @@ Template.main.events({
   //'this' in this context refers to the current thingie
   'click .vote': function (event) {
     event.preventDefault();
-    if(!this.voted && Meteor.user() != null){
-      Thingies.update(this._id,
-      {$set: {vote : this.vote+1}}
-      );
+    if(Meteor.user() != null){
+      Meteor.call('updateVote',this._id,this.vote);
     }
-
-    if(this.voted == false){
-      Thingies.update(this._id,
-      {$set: {voted : true}}
-      );
-    }
-
   },
   'click .delete': function() {
     //only allowed to delete his/her own posts
     try {
-      Meteor.call('match',this.name,this._id);
+      Meteor.call('remove',this.name,this._id);
     } catch(err) {
       console.log("Ignore " + err);
     }
@@ -52,13 +45,7 @@ Template.add.events({
     //prevent defualt submitting of form
     event.preventDefault();
     var newThingie = event.target.thingie.value;
-    Thingies.insert({
-      name: Accounts.user().username || Accounts.user().profile.name,
-      vote: 0,
-      voted: false,
-      thingie: newThingie,
-      when: moment('2015-10-12').fromNow()
-    });
+    Meteor.call('insertData',newThingie);
     //clear form
     event.target.thingie.value = "";
   }
--- server/methods.js
+++ server/methods.js
@@ -1,10 +1,23 @@
+//all my beautiful methods that belong to me
 Meteor.methods({
-  'match': function(name,id){
+  'updateVote' : function(id,currentVote) {
+    Thingies.update(id,
+    {$set: {vote : currentVote+1}}
+    );
+  },
+  'remove': function(name,id){
     try {
       if(name === Accounts.user().username || name === Accounts.user().profile.name)
         Thingies.remove(id);
     } catch(err) {
       console.log("Ignore " + err);
     }
+  },
+  'insertData' : function(thingie) {
+    Thingies.insert({
+      name: Accounts.user().username || Accounts.user().profile.name,
+      vote: 0,
+      thingie: thingie,
+    });
   }
 });
